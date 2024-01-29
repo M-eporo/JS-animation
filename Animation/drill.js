f4ff4311a9001a1938cc049fe3da1f0b9092acdc
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-01-29 11:47:27 +0900

    Update drill.js
    スクロールすると要素が順番に出現追加

--- Animation/drill.js
+++ Animation/drill.js
@@ -1,1 +1,32 @@
+// スクロールすると順番に要素が出現
+function delayScroll(){
+  const time = 0.4;
+  let delay = time;
 
+  $(".delayScroll").each(function() {
+    const elemPos = $(this).offset().top;
+    let height = $(window).height();
+    let scroll = $(window).scrollTop();
+
+    const parent = this;
+    const children = $(parent).children();
+
+    if(scroll >= elemPos - height $$ !$(parent).hasClass("play"){
+      $(".children").each(function(){
+        if(!$(this).hasClass("fadeUp")) {
+          $(parent).addClass("play");
+          $(this).css("animation-delay", `${delay}s`).addClass("fadeUp");
+          delay += time;
+
+          let index = $(children).index(this);
+          if($(children).length - 1 === index) {
+            $(parent).removeClass("play");
+          }
+        }
+      });
+    } else {
+      $(children).removeClass("fadeUp");
+      delay = time;  
+    }
+  });
+}

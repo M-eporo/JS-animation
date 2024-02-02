3d864e93145534d5805fedbbb93d952fbd0a8cc0
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-02-02 14:43:18 +0900

    Add files via upload

---
+++ click-fade-in/script.js
@@ -0,0 +1,26 @@
+const target = document.querySelectorAll(".box");
+console.log(target);
+
+target.forEach((element, index, arry) => {
+
+	element.addEventListener("click", function(e){
+
+		switch(element.dataset.event){
+		case "Left":
+			element.classList.add("fadeLeft");
+			break;
+		case "Right":
+			element.classList.add("fadeRight");
+			break;
+		case "Down":
+			element.classList.add("fadeDown");
+			break;
+		case "Up":
+			element.classList.add("fadeUp");
+			break;
+		default:
+			element.classList.add("fadeIn");
+		}
+		
+	});
+});

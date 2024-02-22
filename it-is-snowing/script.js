fd819e122a8fac92dda1a8ace7212e0321ac5cf7
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-02-22 11:58:04 +0900

    It's snowing

---
+++ it-is-snowing/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+	const container = document.querySelector(".snow-container");
+
+	const createSnow = () => {
+
+	const snowElm = document.createElement("span");
+	snowElm .className = "snow";
+
+	const minSize = 5;
+	const maxSize = 10;
+	const size = Math.random() * (maxSize - minSize) + minSize;
+
+	snowElm.style.width = `${size}px`;
+	snowElm.style.height = `${size}px`;
+	snowElm.style.left = Math.random() * 100 + "%";
+	
+	container.appendChild(snowElm);
+
+	setTimeout( () => {
+		snowElm.remove();
+	}, 10000);
+
+	};
+
+	setInterval(createSnow, 100);
+
+});

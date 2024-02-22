43d2e929b54b071a138e28fb0f51688b9ff20988
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-02-22 15:43:56 +0900

    Add files via upload

---
+++ progressCircle2/script.js
@@ -0,0 +1,37 @@
+var circle = new ProgressBar.Circle('#container', {
+
+	color: '#35f',
+	strokeWidth: 6.0,
+	trailColor: '#888',
+	trailWidth: 1.0,
+	duration: 1000,
+	easing: 'bounce',
+	text: {
+		style: {
+			color: '#333',
+			position: 'absolute',
+			top: '50%',
+			left: '50%',
+			margin: 0,
+			padding: 0,
+			transform: {
+				prefix: true,
+				value: 'translate(-50%, -50%)',
+			}
+			
+		},
+		autoStyleContainer: true,
+	},
+	
+	from: { width: 1, a:0},
+	to: { width: 6, a:1 },
+	step: function(state, circle, attachment) {
+		
+		circle.setText(Math.round(circle.value() * 100) + '%');
+		circle.path.setAttribute('stroke-width', state.width);
+		
+	}
+});
+
+
+circle.animate(1.0);

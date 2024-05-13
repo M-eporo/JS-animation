64726ee8fb7954f9eaa054ccd8ce1b3454b62152
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-13 11:48:34 +0900

    Update script.js

--- click-fade-in/script.js
+++ click-fade-in/script.js
@@ -1,21 +1,21 @@
 const target = document.querySelectorAll(".box");
 target.forEach((element, index, arry) => {
-	element.addEventListener("click", function(e){
-		switch(element.dataset.event){
-		case "Left":
-			element.classList.add("fadeLeft");
-			break;
-		case "Right":
-			element.classList.add("fadeRight");
-			break;
-		case "Down":
-			element.classList.add("fadeDown");
-			break;
-		case "Up":
-			element.classList.add("fadeUp");
-			break;
-		default:
-			element.classList.add("fadeIn");
-		}
-	});
+  element.addEventListener("click", function(e){
+    switch(element.dataset.event){
+      case "Left":
+        element.classList.add("fadeLeft");	
+        break;
+      case "Right":
+        element.classList.add("fadeRight");
+        break;
+      case "Down":
+        element.classList.add("fadeDown");
+        break;
+      case "Up":
+        element.classList.add("fadeUp");
+        break;
+      default:
+        element.classList.add("fadeIn");
+    }
+  });
 });

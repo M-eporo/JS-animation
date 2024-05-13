25a6e34c2c42e22c9e2396ec0b658dc5f47474b2
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-13 11:21:53 +0900

    Update script.js

--- accordion-panel2/script.js
+++ accordion-panel2/script.js
@@ -1,29 +1,25 @@
-//アコーディオンをクリックした時の動作
-$('.title').on('click', function() {//タイトル要素をクリックしたら
-  $('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+$(".title").on("click", function() {
+  $(".box").slideUp(500);
     
-  var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
+  const findElm = $(this).next(".box");
     
-  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-    $(this).removeClass('close');//クラス名を除去    
-  }else{//それ以外は
-    $('.close').removeClass('close'); //クラス名closeを全て除去した後
-    $(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
-    $(findElm).slideDown(500);//アコーディオンを開く
+  if($(this).hasClass("close")){
+    $(this).removeClass("close");
+  }else{
+    $(".close").removeClass("close");
+    $(this).addClass("close");
+    $(findElm).slideDown(500);く
   }
 });
 
 /*
-
-//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
-$(window).on('load', function(){
-  $('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
-  $(".open").each(function(index, element){ //openクラスを取得
-    var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
-    $(Title).addClass('close');       ///タイトルにクラス名closeを付与し
-    var Box =$(element).children('.box'); //openクラスの子要素boxクラスを取得
-    $(Box).slideDown(500);          //アコーディオンを開く
+$(window).on("load", function(){
+  $(".accordion-area li:first-of-type section").addClass("open");
+  $(".open").each(function(index, element){
+    const title =$(element).children(".title");
+    $(title).addClass("close");
+    const box =$(element).children(".box");
+    $(box).slideDown(500);
   });
 });
-
 */

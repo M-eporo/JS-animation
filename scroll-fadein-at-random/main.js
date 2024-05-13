8c8a999ad733d7a98e68a10f726c9c2e4e7ade3f
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-13 13:51:22 +0900

    Update main.js

--- scroll-fadein-at-random/main.js
+++ scroll-fadein-at-random/main.js
@@ -1,43 +1,30 @@
-
-
 function scrollAnimation(){
-	const parent = $(".randomWrapper");
-	const children = $(".randomWrapper").children();
-	randomAnimation();
-
-	function randomAnimation() {
-
-		const position = $(parent).offset().top;
-		let height = $(window).height();
-		let scroll = $(window).scrollTop();
-		
-		if(scroll >= position - height) {
-
-			if(children.length > 0){
-
-				let rnd = Math.floor(Math.random() * children.length);
-						;
-				if(animeFlag){
-					
-					animeFlag = false;
-					
-					$(children[rnd]).addClass("fadeIn");
-					
-					setTimeout(() => {
-						animeFlag = true;
-						randomAnimation();
-					}, 1000);
-
-					children.splice(rnd, 1);
-				}
-				
-			} else {
-				animeFlag = true;
-			}
-		}	
-	}
+  const parent = $(".randomWrapper");
+  const children = $(".randomWrapper").children();
+  randomAnimation();
+  function randomAnimation() {
+    const position = $(parent).offset().top;
+    let height = $(window).height();
+    let scroll = $(window).scrollTop();
+    if(scroll >= position - height) {
+      if(children.length > 0){
+        let rnd = Math.floor(Math.random() * children.length);
+        if(animeFlag){
+          animeFlag = false;			
+          $(children[rnd]).addClass("fadeIn");			
+          setTimeout(() => {
+            animeFlag = true;
+            randomAnimation();
+          }, 1000);
+          children.splice(rnd, 1);
+        }
+      } else {
+      animeFlag = true;	
+      }
+    }
+  }
 }
 let animeFlag = true;
 $(window).on("scroll", function() {
-	scrollAnimation();
+  scrollAnimation();
 });

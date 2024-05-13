25e6d9afd6f9e566c5a07087eee3e56fab3fb80f
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-13 14:45:39 +0900

    Update script.js

--- progressbar/script.js
+++ progressbar/script.js
@@ -1,55 +1,47 @@
 const target = document.querySelectorAll(".box");
-console.log(target);
-
 target.forEach((element, index, arry) => {
-
-	element.addEventListener("click", function(e){
-
-		switch(element.dataset.event){
-		case "Left":
-			element.classList.toggle("fadeLeft");
-			break;
-		case "Right":
-			element.classList.toggle("fadeRight");
-			break;
-		case "Down":
-			element.classList.toggle("fadeDown");
-			break;
-		case "Up":
-			element.classList.toggle("fadeUp");
-			break;
-		default:
-			element.classList.toggle("fadeIn");
-		}
-		
-	});
+  element.addEventListener("click", function(e){
+    switch(element.dataset.event){
+      case "Left":
+        element.classList.toggle("fadeLeft");
+        break;
+      case "Right":
+        element.classList.toggle("fadeRight");
+        break;
+      case "Down":
+        element.classList.toggle("fadeDown");
+        break;
+      case "Up":
+        element.classList.toggle("fadeUp");
+        break;
+      default:
+        element.classList.toggle("fadeIn");
+    }	
+  });
 });
 
-
 var bar = new ProgressBar.Line(splash_text,{
-
-	strokeWidth: 0,
-	duration: 1000,
-	trailWidth: 0,
-	text: {
-		style: {
-			position: 'absolute',
-			left: '50%',
-			top: '50%',
-			padding: '0',
-			margin: '0',
-			transform: 'translate(-50%, -50%)',
-			'font-size': '2rem',
-			color: '#000'
-		},
-
-		autoStyleContainer: false
-	},
-	step: function(state, bar){
-		bar.setText(Math.round(bar.value() * 100) + ' %');
-	}
+  strokeWidth: 0,
+  duration: 1000,
+  trailWidth: 0,
+  text: {
+    style: {
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '0',
+      transform: 'translate(-50%, -50%)',
+      'font-size': '2rem',
+      color: '#000'
+    },
+    autoStyleContainer: false
+  },
+  step: function(state, bar){
+    bar.setText(Math.round(bar.value() * 100) + ' %');
+  }
 });
 
 bar.animate(1.0, function() {
-	$("#splash").delay(500).fadeOut(800);
+  $("#splash").delay(500).fadeOut(800);
 });
